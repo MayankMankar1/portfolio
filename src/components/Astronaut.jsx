cfe6b015c35336372cc8bf259cee7a65ede4e19d
MayankMankar1 <140330421+MayankMankar1@users.noreply.github.com>
2025-08-30 01:34:43 +0530

    ggs

--- src/components/Astronaut.jsx
+++ src/components/Astronaut.jsx
@@ -13,9 +13,8 @@ import { useFrame } from "@react-three/fiber";
 
 export function Astronaut(props) {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF(
-    "/models/tenhun_falling_spaceman_fanart.glb"
-  );
+  const { nodes, materials, animations } = useGLTF(`${import.meta.env.BASE_URL}models/tenhun_falling_spaceman_fanart.glb`);
+
   const { actions } = useAnimations(animations, group);
   useEffect(() => {
     if (animations.length > 0) {
